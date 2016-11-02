db677dac2d2483b2d420193efdcabff7e85ea027
Ruchit Mehta <dharabavishi@gmail.com>
2016-11-02 00:07:49 -0700

    adding files

--- ChatApp/ChatViewController.swift
+++ ChatApp/ChatViewController.swift
@@ -7,13 +7,81 @@
 //
 
 import UIKit
+import Parse
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTextFeild: UITextField!
+    
+    var messages: [PFObject]?
+    
+    @IBAction func sendButtonClick(_ sender: AnyObject) {
+       
+        let messageText  = messageTextFeild.text
+        
+        let message = PFObject(className:"Message")
+        message["text"] = messageText
+        message.saveInBackground {
+            (success: Bool, error: Error?) -> Void in
+            if (success) {
+                // The object has been saved.
+                self.messageTextFeild.text = ""
+            } else {
+                // There was a problem, check error.description
+                print(error?.localizedDescription)
+            }
+        }
 
-class ChatViewController: UIViewController {
 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "Chat"
         // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(reloadMessages), userInfo: nil, repeats: true)
+    }
+    
+    func reloadMessages() {
+        let query = PFQuery(className:"Message")
+        query.order(byDescending: "createAt")
+        query.findObjectsInBackground {
+            (objects: [PFObject]?, error: Error?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) scores.")
+                // Do something with the found objects
+                if let objects = objects {
+                    self.messages = objects
+                    self.tableView.reloadData()
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!)")
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let messages = messages {
+            return messages.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath) as! MessageCell
+        let object = (messages?[indexPath.row])! as PFObject
+        var messageText: String = ""
+        if let text = object["text"] as? String {
+            messageText = text
+        }
+        cell.messageLabel.text = messageText
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
--- ChatApp/LoginViewController.swift
+++ ChatApp/LoginViewController.swift
@@ -7,9 +7,56 @@
 //
 
 import UIKit
+import Parse
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func onLogin(_ sender: AnyObject) {
+        let username = emailTextField.text
+        let password = passwordTextField.text
+        
+        
+        PFUser.logInWithUsername(inBackground: username!, password: password!) { (user: PFUser?, error: Error?) in
+            
+            if let error = error {
+                print("error")
+                print(error.localizedDescription)
+            } else {
+                // Hooray! Let them use the app now.
+                print("success")
+//                let chat = ChatViewController(nibName: "ChatViewController", bundle: nil)
+//                self.present(chat, animated: true, completion: { 
+//                    
+//                })
+                self.performSegue(withIdentifier: "ChatViewController", sender: nil)
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: AnyObject) {
+        let user = PFUser()
+        user.username = emailTextField.text
+        user.password = passwordTextField.text
+        
+        user.signUpInBackground { (succeeded: Bool, error: Error?) -> Void in
+            if let error = error {
+                print("error")
+                //                let errorString = error.userInfo["error"] as? String
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+                print("success")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
